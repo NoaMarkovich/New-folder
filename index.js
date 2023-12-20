7d540775e7e9f23a73857bf4adb710e76ddd09ec
Noa Markowitz <noamarkowitz@gmail.com>
2023-12-20 20:01:04 +0200

    use local storage

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const items = [];
   document.querySelector("form").addEventListener("submit", function (ev) {
     ev.preventDefault();
     console.log("onFormSubmit");
-
     const tr = document.createElement("tr");
     document.querySelector("tbody").appendChild(tr);
-
     /**
      * @type {HTMLInputElement}
      */
@@ -18,13 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
      * @type {HTMLInputElement}
      */
     const who = document.querySelector("#who");
+
+    const item = {
+      what: what.value,
+      when: when.value,
+      who: who.value,
+    };
+    items.push(item);
     tr.innerHTML = `
        <td>${what.value}</td>
        <td>${when.value}</td>
        <td>${who.value}</td>`;
-
     what.value = when.value = who.value = "";
-
     what.focus();
+
+    localStorage.setItem("todo", JSON.stringify(items));
   });
 });
