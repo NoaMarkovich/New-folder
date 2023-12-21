3d71feea8e63403379fec9185d24ca100b713468
Noa Markowitz <noamarkowitz@gmail.com>
2023-12-21 18:42:39 +0200

    restore local storage

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const items = [];
+  // const items = [];
+
+  const itemsJSON = localStorage.getItem("todo");
+
+  const items = JSON.parse(itemsJSON);
+  items.forEach(addRow);
+
+  /**
+   * @type {HTMLInputElement}
+   */
+  const what = document.querySelector("#what");
+  /**
+   * @type {HTMLInputElement}
+   */
+  const when = document.querySelector("#when");
+  /**
+   * @type {HTMLInputElement}
+   */
+  const who = document.querySelector("#who");
+
   document.querySelector("form").addEventListener("submit", function (ev) {
     ev.preventDefault();
     console.log("onFormSubmit");
-    const tr = document.createElement("tr");
-    document.querySelector("tbody").appendChild(tr);
-    /**
-     * @type {HTMLInputElement}
-     */
-    const what = document.querySelector("#what");
-    /**
-     * @type {HTMLInputElement}
-     */
-    const when = document.querySelector("#when");
-    /**
-     * @type {HTMLInputElement}
-     */
-    const who = document.querySelector("#who");
 
     const item = {
       what: what.value,
@@ -24,13 +29,21 @@ document.addEventListener("DOMContentLoaded", function () {
       who: who.value,
     };
     items.push(item);
-    tr.innerHTML = `
-       <td>${what.value}</td>
-       <td>${when.value}</td>
-       <td>${who.value}</td>`;
-    what.value = when.value = who.value = "";
-    what.focus();
 
     localStorage.setItem("todo", JSON.stringify(items));
+
+    addRow(item);
+
+    what.value = when.value = who.value = "";
+    what.focus();
   });
 });
+
+function addRow(item) {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+       <td>${item.what}</td>
+       <td>${item.when}</td>
+       <td>${item.who}</td>`;
+  document.querySelector("tbody").appendChild(tr);
+}
